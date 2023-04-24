0260dc4b49a6e9d9796b166b982ffeaa1cd9fc6b
Ron Hedwig Zape <ronhedwigaquinozape@gmail.com>
2023-04-25 00:47:11 +0800

    Create a modal dialog box for adding notes

--- src/components/Controller.vue
+++ src/components/Controller.vue
@@ -1,18 +1,127 @@
 <template>
-<nav>
-	<ul>
-		<li class="list">Add note</li>
-		<li class="list">Delete note</li>
-	</ul>
-</nav>
-</template>
+	<div class="m-5">
+		<button
+			type="button"
+			@click="openModal"
+			class="rounded-md bg-black bg-opacity-20 px-4 py-2 text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
+		>
+			Add a New Note
+		</button>
+	</div>
+	<TransitionRoot appear :show="isOpen" as="template">
+		<Dialog as="div" @close="closeModal" class="relative z-10">
+			<TransitionChild
+				as="template"
+				enter="duration-300 ease-out"
+				enter-from="opacity-0"
+				enter-to="opacity-100"
+				leave="duration-200 ease-in"
+				leave-from="opacity-100"
+				leave-to="opacity-0"
+			>
+				<div class="fixed inset-0 bg-black bg-opacity-25" />
+			</TransitionChild>
 
-<script>
-export default {
-	name: "Navbar"
+			<div class="fixed inset-0 overflow-y-auto">
+				<div
+					class="flex min-h-full items-center justify-center p-4 text-center"
+				>
+					<TransitionChild
+						as="template"
+						enter="duration-300 ease-out"
+						enter-from="opacity-0 scale-95"
+						enter-to="opacity-100 scale-100"
+						leave="duration-200 ease-in"
+						leave-from="opacity-100 scale-100"
+						leave-to="opacity-0 scale-95"
+					>
+						<DialogPanel
+							class="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all"
+						>
+							<DialogTitle
+								as="h3"
+								class="text-lg font-medium leading-6 text-gray-900"
+							>
+								New Note
+							</DialogTitle>
+							<div class="mt-2">
+								<div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+									<div class="col-span-full">
+										<label
+											for="title"
+											class="block text-sm font-medium leading-1 text-gray-900"
+										>
+											Title
+										</label>
+										<div class="mt-2">
+											<input
+												type="text"
+												name="title"
+												id="title"
+												autocomplete="given-name"
+												class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+											>
+										</div>
+									</div>
+									<div class="col-span-full">
+										<label
+											for="description"
+											class="block text-sm font-medium leading-1 text-gray-900"
+										>
+											Description
+										</label>
+										<div class="mt-2 ">
+											<textarea
+												id="description"
+												name="description"
+												rows="3"
+												class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+											></textarea>
+										</div>
+									</div>
+								</div>
+								<div class="mt-6 flex items-center justify-end gap-x-6">
+<!--									<button-->
+<!--										type="button"-->
+<!--										class="text-sm font-semibold leading-6 text-gray-900"-->
+<!--									>-->
+<!--										Cancel-->
+<!--									</button>-->
+									<button
+										type="submit"
+										class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+									>
+										Add
+									</button>
+								</div>
+							</div>
+
+						</DialogPanel>
+					</TransitionChild>
+				</div>
+			</div>
+		</Dialog>
+	</TransitionRoot>
+</template>
+<script setup>
+import { ref } from 'vue'
+import {
+	TransitionRoot,
+	TransitionChild,
+	Dialog,
+	DialogPanel,
+	DialogTitle,
+} from '@headlessui/vue'
+
+const isOpen = ref(true)
+
+function closeModal() {
+	isOpen.value = false
+}
+function openModal() {
+	isOpen.value = true
 }
 </script>
-
 <style scoped>
 .list {
 	list-style-type: none;
--- src/components/Note.vue
+++ src/components/Note.vue
@@ -19,6 +19,16 @@ const texts = ref([
 	{ content: "A sample note" },
 	{ content: "A sample note" },
 ]);
+
+const getNotes = async () => {
+	try {
+		const response = fetch('');
+		const data = await response.json();
+	}
+	catch (error) {
+		alert(error);
+	}
+}
 </script>
 <style scoped>
 .note {
